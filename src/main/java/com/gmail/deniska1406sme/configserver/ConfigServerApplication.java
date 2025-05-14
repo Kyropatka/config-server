0abd01c19369ac486c9ef958a73dc7048d5a2ffb
Denys Smelnytskyi <deniska1406sme@gmail.com>
2025-05-14 21:11:01 +0300

    init

---
+++ src/main/java/com/gmail/deniska1406sme/configserver/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.gmail.deniska1406sme.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
